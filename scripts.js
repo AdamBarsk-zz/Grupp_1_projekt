c6794ae1ddda2b26e8ac1d047f6f7fa2f8913b41
Victor Bergström <victor_bergstrom@hotmail.com>
2016-10-11 15:01:00 +0200

    hjk

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 
 // Gallery slider
+function startZoom(){
+  var zoombg = document.getElementById('black')
+  var zoom = document.getElementsByClassName('zoom')
+
+  zoombg.style.display = "block"
+  zoom.style.display = "block"
+}
