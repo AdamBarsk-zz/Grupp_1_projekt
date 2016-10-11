fbf3ba77ddf8bc6daf50d579cb15e8719abf96d9
AdamBarsk <adambarsk@gmail.com>
2016-10-11 14:55:07 +0200

    JS klar

--- scripts.js
+++ scripts.js
@@ -7,3 +7,11 @@ function startZoom(){
   zoombg.style.display = "block"
   zoom.style.display = "block"
 }
+
+function stopZoom(){
+  var zoombg = document.getElementById('black')
+  var zoom = document.getElementsByClassName('zoom')
+
+  zoombg.style.display = "none"
+  zoom.style.display = "none"
+}
