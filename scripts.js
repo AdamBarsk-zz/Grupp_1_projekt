9fdd141f63d15d05935ee037a1448ea3810ccb80
Victor Bergström <victor_bergstrom@hotmail.com>
2016-10-12 10:52:59 +0200

    bildgalleri provar window.onload

--- scripts.js
+++ scripts.js
@@ -2,23 +2,33 @@
 // GALLERY SLIDER
 
 //Startar slidern
+window.onload = function (){
+
 document.getElementById('preview').addEventListener('click', startZoom);
 
 function startZoom(){
   var zoombg = document.getElementById('black');
   var zoomed = document.getElementById('zoom');
+  var previewHide = document.getElementById('preview');
 
   zoombg.style.display = "block";
   zoomed.style.display = "block";
+  previewHide.style.display = "none";
+}
 }
 
 //Avslutar slidern
+window.onload = function (){
+
 document.getElementById('black').addEventListener('click', stopZoom);
 
 function stopZoom(){
   var zoombg = document.getElementById('black')
-  var zoom = document.getElementById('soom')
+  var zoom = document.getElementById('zoom')
+  var previewHide = document.getElementById('preview');
 
   zoombg.style.display = "none"
   zoom.style.display = "none"
+  previewHide.style.display = "block";
+}
 }
