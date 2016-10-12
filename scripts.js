ce839723b7b318666365939c7badbec0f9803b12
AdamBarsk <adambarsk@gmail.com>
2016-10-12 08:50:12 +0200

    hej

--- scripts.js
+++ scripts.js
@@ -1,23 +1,10 @@
 
 // Gallery slider
-$('.preview').click(startZoom());
+
+document.getElementsByClassName('preview').addEventListener("click", startZoom);
 
 function startZoom(){
-  var zoombg = document.getElementById('black')
-  var zoom = document.getElementsByClassName('zoom')
 
-  alert("bajs")
+  alert("hej");
 
-  zoombg.style.display = "block"
-  zoom.style.display = "block"
-}
-
-
-function stopZoom(){
-  var zoombg = document.getElementById('black')
-  var zoom = document.getElementsByClassName('zoom')
-
-  zoombg.style.display = "none"
-  zoom.style.display = "none"
-}
-
+};
