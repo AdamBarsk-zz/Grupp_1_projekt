64994ad017a8df55aed060501852b11ac697a828
AdamBarsk <adambarsk@gmail.com>
2016-10-13 08:58:02 +0200

    bilder funkar i popup

--- scripts.js
+++ scripts.js
@@ -2,13 +2,11 @@
 // GALLERY SLIDER
 
 //Startar slidern
-var falukorv = document.getElementsByClassName('preview');
+var falukorv = document.getElementsByClassName('flashy');
 
 for (var i = 0; i < falukorv.length; i++) {
 	falukorv[i].addEventListener('click', startZoom);
 }
-
-var imgArray = [].slice.call(falukorv);
 
 
 
@@ -17,11 +15,10 @@ function startZoom(){
   var zoombg = document.getElementById('black');
 	var previewHide = document.getElementsByClassName('preview');
 	var div = document.getElementById('white')
-	div.innerHTML += "Hej"
+	div.innerHTML = "<img src='"+this.src+"' />"
 
   zoombg.style.display = "block";
 	div.style.display = "block";
-
 }
 
 //Avslutar slidern
@@ -30,7 +27,8 @@ document.getElementById('black').addEventListener('click', stopZoom);
 function stopZoom(){
   var zoombg = document.getElementById('black')
   var previewHide = document.getElementById('preview');
+	var div = document.getElementById('white')
 
   zoombg.style.display = "none"
-
+	div.style.display = "none";
 }
