452a8474d841963143c3275d5bb07871ff3abfc8
AdamBarsk <adambarsk@gmail.com>
2016-10-13 16:10:28 +0200

    arrows

--- scripts.js
+++ scripts.js
@@ -15,6 +15,9 @@ function startZoom(){
   var zoombg = document.getElementById('black');
 	var div = document.getElementById('white')
 	div.innerHTML = "<img src='"+this.src+"' />"
+	 + '<span class="glyphicon glyphicon-chevron-left" id="left-arrow"></span>'
+	 + '<span class="glyphicon glyphicon-chevron-right" id="right-arrow"></span>'
+
 
   zoombg.style.display = "block";
 	div.style.display = "block";
