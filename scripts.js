8a69ac19ba394a4d325b0ce67bfc629aa347bf37
AdamBarsk <adambarsk@gmail.com>
2016-10-13 11:22:45 +0200

    hej

--- scripts.js
+++ scripts.js
@@ -13,7 +13,6 @@ for (var i = 0; i < falukorv.length; i++) {
 
 function startZoom(){
   var zoombg = document.getElementById('black');
-	var previewHide = document.getElementsByClassName('preview');
 	var div = document.getElementById('white')
 	div.innerHTML = "<img src='"+this.src+"' />"
 
@@ -26,7 +25,6 @@ document.getElementById('black').addEventListener('click', stopZoom);
 
 function stopZoom(){
   var zoombg = document.getElementById('black')
-  var previewHide = document.getElementById('preview');
 	var div = document.getElementById('white')
 
   zoombg.style.display = "none"
