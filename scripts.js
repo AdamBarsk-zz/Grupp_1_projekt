b6f8e94f70aaba876724cfc34d99bbe2c2ea2afa
AdamBarsk <adambarsk@gmail.com>
2016-10-17 10:52:00 +0200

    hej

--- scripts.js
+++ scripts.js
@@ -8,13 +8,18 @@ for (var i = 0; i < falukorv.length; i++) {
 	falukorv[i].addEventListener('click', startZoom);
 }
 
+var images = [];
+images.push(falukorv);
+console.log(images);
+
 function startZoom(){
   var zoombg = document.getElementById('black');
 	var div = document.getElementById('white')
-	div.innerHTML += "<img src='"+this.src+"' />"
+	div.innerHTML = "<img src='"+this.src+"' id='test2' />"
+	 + '<span class="glyphicon glyphicon-chevron-left" id="left-arrow"></span>'
+	 + '<span class="glyphicon glyphicon-chevron-right" id="right-arrow"></span>'
 
-
-
+	 listener();
   zoombg.style.display = "block";
 	div.style.display = "block";
 }
@@ -32,18 +37,27 @@ function stopZoom(){
 }
 
 // Change image
+function listener(){
+	$("span.glyphicon").on('click', move);
+	function move()
+	{
 
-$("#left-arrow").on('click', move);
-function move()
-{
-	console.log("hej")
 	    // //Om span class= left i--
-	    // if (this.id == 'left') { i--; }
+	    if (this.id == 'left-arrow') {
+				 i--;
+			 }
 	    //     // Om span class=right i++
-	    // else if (this.id == 'right') { i++; }
+	    else if (this.id == 'right-arrow') {
+				 i++;
+		 	}
 			//
 	    // // OM i<0 eller i > images.length-1 då ändra i till godkänt värde
-	    // if (i < 0) { i = images.length - 1; }
-	    // if (i > (images.length - 1)) { i = 0;}
-	    // $("#myImg").prop('src', images[i].src);//Visa bild
+	    if (i < 0) {
+				 i = falukorv.length - 1;
+			 }
+	    if (i > (falukorv.length - 1)) {
+				 i = 0;
+			 }
+	    $("#test2").prop('src', falukorv[i].src);//Visa bild
+	}
 }
