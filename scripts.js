6f12501c4ad739d3517a761da0ffe8950a7277f4
AdamBarsk <adambarsk@gmail.com>
2016-10-17 16:21:41 +0200

    bildgalleri klart

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 // GALLERY SLIDER
 
 //Startar slidern
+var active_image;
 var falukorv = document.getElementsByClassName('flashy');
 var images = [];
 for (var i = 0; i < falukorv.length; i++) {
@@ -10,19 +11,15 @@ for (var i = 0; i < falukorv.length; i++) {
 	images[i].addEventListener('click', startZoom);
 }
 
-
-
-console.log(images);
-
-
-function startZoom(){
+function startZoom(event){
   var zoombg = document.getElementById('black');
 	var div = document.getElementById('white');
-	div.innerHTML = "<img src='"+this.src+"' id='test2' />"
+	active_image = event.target.id;
+	div.innerHTML = "<img src='"+event.target.src+"' id='test2' />"
 	 + '<span class="glyphicon glyphicon-chevron-left" id="left-arrow"></span>'
 	 + '<span class="glyphicon glyphicon-chevron-right" id="right-arrow"></span>'
-	 console.log(this.src)
-	listener();
+	console.log(event.target)
+	listener(event.target);
   zoombg.style.display = "block";
 	div.style.display = "block";
 }
@@ -39,33 +36,24 @@ function stopZoom(){
 	div.style.display = "none";
 }
 
-
 // Change image
-function listener(){
+function listener(e){
 	$("span.glyphicon").on('click', move);
-	function move(e)
-	{
-
-
-	    // //Om span class= left i--
-	    if (this.id == 'left-arrow') {
-
-				 i--;
-			 }
-	    //     // Om span class=right i++
-	    else if (this.id == 'right-arrow') {
-				 i++;
-		 	}
-
-	    // When backing from first image, go to last, and vice versa.
-	    if (i < 0) {
-				 i = images.length - 1;
-			 }
-	    if (i > (images.length) - 1) {
-				 i = 0;
-			 }
-	    $("#test2").prop('src', images[i].src);//Visa bild
-			console.log(images[i]);
-			// console.log(event.target);
+	i=active_image-1;
+	function move(){
+		if (this.id == 'left-arrow') {
+			i--;
+		} else if (this.id == 'right-arrow') {
+			i++;
+		}
+		if (i < 0) {
+		 i = images.length - 1;
+		}
+		if (i > (images.length) - 1) {
+			i = 0;
+		}
+		$("#test2").prop('src', images[i].src);
+		console.log(images[i]);
+		console.log(e)
 	}
 }
