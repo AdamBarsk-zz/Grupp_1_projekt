f34368a41b296607735498b9af525fa432468174
Jacob Fredriksson <jacobfredriksson@live.se>
2016-10-18 19:02:05 +0200

    la till dagens datum i kalender inputarna

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,6 +9,21 @@ $(function() {
   });
 });
 
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+document.getElementById("checkin").value = today;
+document.getElementById("checkout").value = today;
+
+
+
 var customers = [];
 
 function mySubmit() {
