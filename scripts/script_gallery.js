c4faf498c1995981b7c7942ea656d4bdc51a0462
AdamBarsk <adambarsk@gmail.com>
2016-10-19 14:49:09 +0200

    får duga för nu

--- scripts/script_gallery.js
+++ scripts/script_gallery.js
@@ -1,26 +1,42 @@
 //Make array of the thumbnails
-
-var imgArray = $('flashy').toArray();
+var imgArray = $('.flashy').toArray();
 
 //Pop up carousel when clicking thumbnail
 $(".flashy").click(function(){
   var leftArrow = $("<span class='glyphicon glyphicon-chevron-left' id='left-arrow'></span>");
-	var rightArrow = $("<span class='glyphicon glyphicon-chevron-right' id='right-arrow'></span>");
-	var image = $("<img src='" + event.target.src + "' id='slider' class='active' />");
+  var rightArrow = $("<span class='glyphicon glyphicon-chevron-right' id='right-arrow'></span>");
+  var image = $("<img src='" + event.target.src + "' id='slider' />");
   $("#white").empty(); //Refreshing/empty pop-up on every click
   $("#black").css("display", "block"); //Make black bg visible
   $("#white").css("display", "block"); //Make pop-up visible
   $("#white").append(image, leftArrow, rightArrow); //Fill pop-up with img and arrows
-  listener(event.target);
+  imgIndex = $('.flashy').index(this);
+  slide(imgIndex);
+  console.log(imgIndex);
 });
+
+// Change image
+function slide(e){
+ $("span.glyphicon").on('click', move);
+ i=imgIndex;
+ function move(){
+ 	if (this.id == 'left-arrow') {
+ 		i--;
+ 	} else if (this.id == 'right-arrow') {
+ 		i++;
+ 	}
+ 	if (i < 0) {
+ 	 i = imgArray.length - 1;
+ 	}
+ 	if (i > (imgArray.length) - 1) {
+ 		i = 0;
+ 	}
+ 	$("#slider").prop('src', imgArray[i].src);
+ }
+}
 
 //Hides carousel
 $("#black, #footer").click(function(){
   $("#black").css("display", "none"); //Hide the black bg
   $("#white").css("display", "none"); //Hide pop-up div
 });
-
-//Slidefunction
-$("span.glyphicon").click(listener(event.target){
-  		console.log(event.target);
-	});
