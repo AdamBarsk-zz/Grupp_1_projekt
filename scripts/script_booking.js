050fd3e67e60562a4dbce52c1861d4353d30d2d6
Max <max.trewhitt@student.kyh.se>
2016-10-24 10:50:35 +0200

    autocomplete off färre rutor att fylla i bokning

--- scripts/script_booking.js
+++ scripts/script_booking.js
@@ -41,20 +41,16 @@ function mySubmit() {
 		outCheck: 				document.getElementsByTagName("input")[1].value,
     doubleBeds:       document.getElementsByTagName("input")[2].value,
     doubleBeds:       document.getElementsByTagName("input")[3].value,
-		firstName: 				document.getElementsByTagName("input")[4].value,
-		lastName: 				document.getElementsByTagName("input")[5].value,
-		email: 						document.getElementsByTagName("input")[6].value,
-		adress: 					document.getElementsByTagName("input")[7].value,
-		phoneNumber: 			document.getElementsByTagName("input")[8].value,
+		name: 				    document.getElementsByTagName("input")[4].value,
+		email: 						document.getElementsByTagName("input")[5].value,
+		phoneNumber: 			document.getElementsByTagName("input")[6].value,
 		specialRequests: 	document.getElementsByTagName("textarea")[0].value
 	};
 	customers.push(customer);
 	localStorage.setItem("inCheck", customer.inCheck);
 	localStorage.setItem("outCheck", customer.outCheck);
-	localStorage.setItem("firstName", customer.firstName);
-	localStorage.setItem("lastName", customer.lastName);
+	localStorage.setItem("name", customer.name);
 	localStorage.setItem("email", customer.email);
-	localStorage.setItem("adress", customer.adress);
 	localStorage.setItem("phoneNumber", customer.phoneNumber);
 	localStorage.setItem("specialRequests", customer.specialRequests);
 	return false;
