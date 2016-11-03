f4e27bc25b9897135e560b79576ff074d75c37bf
Victor Bergström <victor_bergstrom@hotmail.com>
2016-11-03 11:15:16 +0100

    senaste versionen

--- scripts/script_gallery.js
+++ scripts/script_gallery.js
@@ -6,10 +6,10 @@ $(".flashy").click(function(){
   var leftArrow = $("<span class='glyphicon glyphicon-chevron-left' id='left-arrow'></span>");
   var rightArrow = $("<span class='glyphicon glyphicon-chevron-right' id='right-arrow'></span>");
   var image = $("<img src='" + event.target.src + "' id='slider' />");
-  $("#img_placeholder").empty(); //Refreshing/empty pop-up on every click
-  $("#overlay").css("display", "block"); //Make overlay bg visible
-  $("#img_placeholder").css("display", "block"); //Make pop-up visible
-  $("#img_placeholder").append(image, leftArrow, rightArrow); //Fill pop-up with img and arrows
+  $("#white").empty(); //Refreshing/empty pop-up on every click
+  $("#black").css("display", "block"); //Make black bg visible
+  $("#white").css("display", "block"); //Make pop-up visible
+  $("#white").append(image, leftArrow, rightArrow); //Fill pop-up with img and arrows
   imgIndex = $('.flashy').index(this);
   slide(imgIndex);
   console.log(imgIndex);
@@ -37,7 +37,7 @@ function slide(e){
 }
 
 //Hides carousel
-$("#overlay, #footer").click(function(){
-  $("#overlay").css("display", "none"); //Hide the overlay bg
-  $("#img_placeholder").css("display", "none"); //Hide pop-up div
+$("#black, #footer").click(function(){
+  $("#black").css("display", "none"); //Hide the black bg
+  $("#white").css("display", "none"); //Hide pop-up div
 });
