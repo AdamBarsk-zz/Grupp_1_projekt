78083e84d94572130e373f2857952e867a774d7c
Max <max.trewhitt@student.kyh.se>
2016-11-03 10:10:17 +0100

    tog bort "aktiviteter"

--- confirmation.php
+++ confirmation.php
@@ -54,7 +54,6 @@ if($_POST) {
 					<div class="collapse navbar-collapse" id="myNavbar">
 						<ul id="ul" class="nav navbar-nav">
 							<li><a id="a" href="index.html">Hem</a></li>
-							<li><a href="#">Aktiviteter</a></li>
 							<li><a id="a2" href="booking.html">Boka</a></li>
 							<li><a id="a3" href="gallery.html">Galleri</a></li>
 							<li><a id="a4" href="#">Kontakt</a></li>
