4aeda7aec0a846343bbf45f1e4f4a9894ce8caf6
Max <max.trewhitt@student.kyh.se>
2016-11-08 11:36:59 +0100

    Bokningssida laddar in värden, även fast tomma

--- scripts/script_booking.js
+++ scripts/script_booking.js
@@ -54,8 +54,10 @@ document.getElementById("checkout").value = tomorrow;
 // Form logic
 var customers = [];
 var inputs = document.getElementsByTagName("input");
+var textarea = document.getElementsByTagName("textarea")[0];
 
 window.onload = function() {
+  // Load saved values
   Load();
 };
 
@@ -64,6 +66,7 @@ for (var i = 0; i < inputs.length; i++) {
 }
 
 function mySubmit() {
+  // Check if demands are met
   validate();
 }
 function Load() {
@@ -71,17 +74,17 @@ function Load() {
   inputs[1].value = localStorage.getItem("outCheck");
   inputs[2].value = localStorage.getItem("doubleBeds");
   inputs[3].value = localStorage.getItem("singleBeds");
-  inputs[4].value = localStorage.getItem("inCheck");
-  inputs[5].value = localStorage.getItem("outCheck");
-  inputs[6].value = localStorage.getItem("doubleBeds");
-  inputs[7].value = localStorage.getItem("singleBeds");
+  inputs[4].value = localStorage.getItem("name");
+  inputs[5].value = localStorage.getItem("email");
+  inputs[6].value = localStorage.getItem("phoneNumber");
+  inputs[0].value = localStorage.getItem("specialRequests");
 }
 function Save(index) {
   var customer = {
 		inCheck: 					document.getElementsByTagName("input")[0].value,
 		outCheck: 				document.getElementsByTagName("input")[1].value,
     doubleBeds:       document.getElementsByTagName("input")[2].value,
-    doubleBeds:       document.getElementsByTagName("input")[3].value,
+    singleBeds:       document.getElementsByTagName("input")[3].value,
 		name: 				    document.getElementsByTagName("input")[4].value,
 		email: 						document.getElementsByTagName("input")[5].value,
 		phoneNumber: 			document.getElementsByTagName("input")[6].value,
