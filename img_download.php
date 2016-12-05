cc2ad798bc3755fd1b41ab7a2262249d3bc6533d
Victor Bergström <victor_bergstrom@hotmail.com>
2016-12-05 15:22:00 +0100

    admin gallery functions and layouts complete

--- img_download.php
+++ img_download.php
@@ -1,6 +1,37 @@
 <?php
-$dir = "uploads/*";
+$dir = "images/uploads/*";
 foreach(glob($dir) as $file)   {   
-	echo "<img class='img-rounded flashy' src='$file' alt='$file'/>";   
+	if (!isset($_SESSION['admin'])) {
+	
+	echo "<form method='post' action='gallery.php' class='form-group image_container'>
+			<img class='img-rounded flashy' src='$file' alt='$file'/>
+			</form>";
+	
+	echo '<style>
+			.image_container {display: inline-block};
+		 </style>';   
+	}
+
+	if (isset($_SESSION['admin'])) {
+	
+	echo "<form method='post' action='gallery.php' class='form-group image_container' style='display: inline-block;'>
+			<img class='img-rounded flashy' src='$file' alt='$file'/><br> 
+			<input name='delete' type='hidden' value='" .$file. "'>
+			<button class='btn btn-danger delete_button'>Radera</button>
+			</form>";
+	
+	echo '<style>
+			.image_container {display: inline-block};
+		 </style>';   
+	}
+
+	$filename = $_POST['delete'];
+
+	if(isset($_POST['delete'])) { 
+		unlink($filename);
+		header('location: http://www.dengladageten.se/gallery.php');
+	}
+
+
 }
 ?>
