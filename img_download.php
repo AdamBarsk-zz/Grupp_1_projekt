5f25ac5e56231c9ada5e3f755204ec9c2bdb4b69
Victor Bergström <victor_bergstrom@hotmail.com>
2016-12-06 10:22:37 +0100

    if/else-syntax fixed

--- img_download.php
+++ img_download.php
@@ -1,28 +1,27 @@
 <?php
 $dir = "images/uploads/*";
 foreach(glob($dir) as $file)   {   
-	if (!isset($_SESSION['admin'])) {
-	
-	echo "<form method='post' action='gallery.php' class='form-group image_container'>
-			<img class='img-rounded flashy' src='$file' alt='$file'/>
-			</form>";
-	
-	echo '<style>
-			.image_container {display: inline-block};
-		 </style>';   
-	}
 
 	if (isset($_SESSION['admin'])) {
 	
-	echo "<form method='post' action='gallery.php' class='form-group image_container' style='display: inline-block;'>
-			<img class='img-rounded flashy' src='$file' alt='$file'/><br> 
-			<input name='delete' type='hidden' value='" .$file. "'>
-			<button class='btn btn-danger delete_button'>Radera</button>
-			</form>";
-	
-	echo '<style>
-			.image_container {display: inline-block};
-		 </style>';   
+		echo "<form method='post' action='gallery.php' class='form-group image_container' style='display: inline-block;'>
+				<img class='img-rounded flashy' src='$file' alt='$file'/><br> 
+				<input name='delete' type='hidden' value='" .$file. "'>
+				<button class='btn btn-danger delete_button'>Radera</button>
+				</form>";
+		
+		echo '<style>
+				.image_container {display: inline-block};
+			 </style>';   
+	} else {
+
+		echo "<form method='post' action='gallery.php' class='form-group image_container'>
+				<img class='img-rounded flashy' src='$file' alt='$file'/>
+				</form>";
+		
+		echo '<style>
+				.image_container {display: inline-block};
+			 </style>';   
 	}
 
 	$filename = $_POST['delete'];
