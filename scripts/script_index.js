458f67bc4fe5fcd6b0d4678f11d81528d7eea333
Jacob Fredriksson <jacobfredriksson@live.se>
2016-12-12 15:02:58 +0100

    lints

--- scripts/script_index.js
+++ scripts/script_index.js
@@ -1,4 +1,3 @@
-
 $(function() {
   $("#check-in-date").datepicker({
       inline:true,
@@ -6,14 +5,7 @@ $(function() {
       firstDay: 1,
       dateFormat: "yy-mm-dd",
       dayNamesMin: ["Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"],
-      minDate: today,
-      onSelect: function (date) {
-          var date2 = $("#check-in-date").datepicker("getDate");
-          date2.setDate(date2.getDate() + 1);
-          $("#check-out-date").datepicker("setDate", date2);
-          //sets minDate to checkin date + 1
-          $("#check-out-date").datepicker("option", "minDate", date2);
-      }
+      minDate: today
   });
   $("#check-out-date").datepicker({
       inline:true,
@@ -21,16 +13,7 @@ $(function() {
       firstDay: 1,
       dateFormat: "yy-mm-dd",
       dayNamesMin: ["Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"],
-      minDate: tomorrow,
-      onClose: function () {
-          var checkin = $("#check-in-date").datepicker("getDate");
-          var checkout = $("#check-out-date").datepicker("getDate");
-          //check to prevent a user from entering a date below date of checkin
-          if (checkout <= checkin) {
-              var minDate = $("#check-out-date").datepicker("option", "minDate");
-              $("#check-out-date").datepicker("setDate", minDate);
-          }
-      }
+      minDate: tomorrow
   });
 });
 
@@ -42,14 +25,14 @@ var month = date.getMonth() + 1;
 var tomorrowMonth = date.getMonth() + 1;
 var year = date.getFullYear();
 
-if (month < 10) month = "0" + month;
-if (day < 10) day = "0" + day;
-if (tomorrowMonth < 10) tomorrowMonth = "0" + tomorrowMonth;
-if (tomorrow < 10) tomorrow = "0" + tomorrow;
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
+  if (tomorrowMonth < 10) tomorrowMonth = "0" + tomorrowMonth;
+  if (tomorrow < 10) tomorrow = "0" + tomorrow;
 
 
 var today = year + "-" + month + "-" + day;
-var tomorrow = year + "-" + month + "-" + tomorrow;
+var tomorrow = year + "-" + tomorrowMonth + "-" + tomorrow;
 
 document.getElementById("check-in-date").value = today;
 document.getElementById("check-out-date").value = tomorrow;
