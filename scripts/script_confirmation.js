59e4356950cd9a068a7e1bab7a2a15f9e88825f3
Max <max.trewhitt@student.kyh.se>
2016-12-19 13:35:17 +0100

    småfix i confirmation

--- scripts/script_confirmation.js
+++
@@ -1,9 +0,0 @@
-function abort() {
-	bookingdiv.style.display = "none";
-	abortdiv.style.display = "block";
-
-	// after 4 seconds, go back to home page
-	setTimeout(function() {
-		window.location.href = "/index.php";
-	}, 4000);
-}
