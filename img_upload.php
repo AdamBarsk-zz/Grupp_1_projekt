a762cc53b58b57c2a0bb0ca635929556ba74441f
Ammelie <ammelie.neth@student.kyh.se>
2016-12-20 14:53:45 +0100

    img_upload lint

--- img_upload.php
+++ img_upload.php
@@ -8,19 +8,40 @@ if (isset($_POST["add"])) {
 
 	if (file_exists($target_file)) {
 		echo '<link rel="stylesheet" type="text/css" href="styles.css" />';
-		echo "<div><p class='failToUpload'> Denna get finns redan! </p></div>";
+		echo "<div>
+						<p class='failToUpload'>
+							Denna bild finns redan!
+						</p>
+					</div>";
 		$uploadOk = 0;
 	} else if ($_FILES["fileToUpload"]["size"] >= 4194304) {
-		echo "<div><p class='failToUpload'> Tyvärr, geten är för stor. Högsta mankhöjd är 4 MB.</p></div>";
+			echo "<div>
+							<p class='failToUpload'>
+								Tyvärr, bilden är för stor. Största tillåtna filstorlek är 4 MB.
+							</p>
+						</div>";
 		$uploadOk = 0;
 	} else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-				echo "<div><p class='failToUpload'>Bara getter med efternamn JPG, JPEG, PNG & GIF finns på denna gård.</p></div>";
-				$uploadOk = 0;
-			} else if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-						echo "<div><p class='uploadSuccess'> Geten: ". "'" . basename ( $_FILES["fileToUpload"]["name"]) . "'" . " har ladddats upp. </p></div>";	
-					} else {
-						echo "<div><p class='failToUpload'>Tyvärr, geten blev förvirrad på vägen och försvann.</p></div>";
-	  				  };
+			echo "<div>
+							<p class='failToUpload'>
+								Kontrollera att bilden har en tillåten filändelse: JPG, JPEG, PNG & GIF.
+							</p>
+						</div>";
+		$uploadOk = 0;
+	} else if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			echo "<div>
+							<p class='uploadSuccess'>
+								Bilden har laddats upp.
+							</p>
+						</div>";
+	} else {
+			echo "<div>
+							<p class='failToUpload'>
+								Tyvärr, någonting gick fel. Försök igen!
+							</p>
+						</div>";
+		$uploadOk = 0;
+	};
 }
 
 $filename = $_POST['delete'];
