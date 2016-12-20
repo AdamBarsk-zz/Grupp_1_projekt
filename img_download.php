1facd2eb3a98c1941c25fe8cb0dd05f6556bdcea
Victor Bergström <victor_bergstrom@hotmail.com>
2016-12-20 14:57:24 +0100

    script booking LINTED

--- img_download.php
+++ img_download.php
@@ -2,21 +2,15 @@
 $dir = "images/uploads/*";
 foreach(glob($dir, GLOB_NOSORT) as $file)   {
 	if (isset($_SESSION['admin'])) {
-		echo "<form method='post' action='gallery.php' class='form-group image_container' style='display: inline-block;'>
+		echo "<form method='post' action='gallery.php' class='form-group image_container'>
 				<img class='img-rounded flashy' src='$file' alt='$file'/><br>
 				<input name='delete' type='hidden' value='" .$file. "'>
 				<button class='btn btn-danger delete_button'>Radera</button>
 			  </form>";
-		echo '<style>
-				.image_container {display: inline-block};
-			 </style>';
 	} else {
 		echo "<form method='post' action='gallery.php' class='form-group image_container'>
 				<img class='img-rounded flashy' src='$file' alt='$file'/>
 			  </form>";
-		echo '<style>
-				.image_container {display: inline-block};
-			 </style>';
 	}
 }
 ?>
