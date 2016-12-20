840e87d48d94cafebcff1866daaa5de202aed0c1
AdamBarsk <adambarsk@gmail.com>
2016-12-20 14:37:07 +0100

    lint

--- img_download.php
+++ img_download.php
@@ -1,25 +1,22 @@
 <?php
 $dir = "images/uploads/*";
-foreach(glob($dir, GLOB_NOSORT) as $file)   {   
+foreach(glob($dir, GLOB_NOSORT) as $file)   {
 	if (isset($_SESSION['admin'])) {
-	
 		echo "<form method='post' action='gallery.php' class='form-group image_container' style='display: inline-block;'>
-				<img class='img-rounded flashy' src='$file' alt='$file'/><br> 
+				<img class='img-rounded flashy' src='$file' alt='$file'/><br>
 				<input name='delete' type='hidden' value='" .$file. "'>
 				<button class='btn btn-danger delete_button'>Radera</button>
 			  </form>";
-		
 		echo '<style>
 				.image_container {display: inline-block};
-			 </style>';   
+			 </style>';
 	} else {
 		echo "<form method='post' action='gallery.php' class='form-group image_container'>
 				<img class='img-rounded flashy' src='$file' alt='$file'/>
 			  </form>";
-		
 		echo '<style>
 				.image_container {display: inline-block};
-			 </style>';   
+			 </style>';
 	}
 }
 ?>
